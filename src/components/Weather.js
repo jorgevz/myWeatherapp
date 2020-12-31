34d064f3d1d3648af248bf5b04e0e72a32631e5a
jorgevz <jorgev2132@gmail.com>
2020-12-31 11:50:46 -0400

    api query is up and running

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -8,37 +8,57 @@ export default class Weather extends React.Component {
         super(props)
         this.state = {
           weather: [],
+          city: ''
+          
         
        }
     }
 
 
 
-async componentDidMount(){
+handlenum1Change (event) {
+
+  let city = (event.target.value);
   
-axios.get(`http://api.weatherapi.com/v1/current.json?key=b61abddeaf714b5bba9210052201012&q=nyc`).then
-( res => {
-   
-   this.setState ({weather: [res.data] })
-    
+  console.log(city)
+  }
+  
+
+getWeather = async (e) => {
+  e.preventDefault();
+  
+  const city = document.getElementById('city').value;
+
+ axios.get(`http://api.weatherapi.com/v1/current.json?key=b61abddeaf714b5bba9210052201012&q=${city}`).then(
+   res => {
+    this.setState ({weather: [res.data] })
     console.log(this.state.weather)
-
-        
-});
-}
-
+   }
+ );
   
 
-  
-  
  
+}
 
 render (){
 
 return(
-<div>
-  
-<h2>{this.state.weather.map(weather => (<div>
+<div id='all' >
+ 
+<form onSubmit={this.getWeather}>
+<input
+type='text' 
+placeholder='City...'
+id='city'
+onChange={this.handlenum1Change}
+/>
+
+
+<button>Get Weather</button>
+
+
+  <br/>
+  <h2>{this.state.weather.map(weather => (<div>
   {weather.location.name}
   <br/>
   {weather.location.country}
@@ -50,12 +70,8 @@ return(
   <h2>{weather.current.condition.text}</h2>
   <br/>
   </div>))}</h2>
-
+  </form>
 </div>
-
-
-
-    
 
 
 )
